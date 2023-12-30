1b8bbc8c9facc502fe805594c8d92adae0e44063
Yannick Simon <yannick.simon@gmail.com>
2023-12-30 13:06:15 +0100

    wip sections

--- lib/models/model_formulaire.dart
+++ lib/models/model_formulaire.dart
@@ -12,7 +12,7 @@ class Formulaire {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['form_on_site_uuid'] = form_on_site_uuid;
     data['form_name'] = form_name;
-
+    data['section'] = ConvertListSectionsToJson(sections);
     return data;
   }
 
@@ -20,5 +20,34 @@ class Formulaire {
       : form_on_site_uuid = json.containsKey('form_on_site_uuid')
             ? json['form_on_site_uuid']
             : generateUUID(),
-        form_name = json['form_name'] as String;
+        form_name = json['form_name'] as String,
+        sections = ConvertJsonToMapSections(json['sections']);
+}
+
+Map<String, Section> ConvertJsonToMapSections(map) {
+  Map<String, Section> res = {};
+
+  map.forEach((key, value) {
+    Section s = Section(
+        section_name: value["section_name"],
+        section_type: value["section_type"],
+        section_on_site_uuid: value.containsKey('section_on_site_uuid')
+            ? value["section_on_site_uuid"]
+            : generateUUID());
+
+    res[key] = s;
+  });
+  // }
+  return res;
+}
+
+Map<String, dynamic> ConvertListSectionsToJson(Map<String, Section> map) {
+  Map<String, dynamic> res = {};
+
+  map.forEach((key, value) {
+    dynamic json = value.toJSON();
+    res[key] = json;
+  });
+  // }
+  return res;
 }
--- lib/models/model_section.dart
+++ lib/models/model_section.dart
@@ -3,14 +3,18 @@ import '../ui/utils/uuid.dart';
 class Section {
   String section_on_site_uuid;
   String section_name;
+  String section_type;
 
-  Section({required this.section_on_site_uuid, required this.section_name});
+  Section(
+      {required this.section_on_site_uuid,
+      required this.section_name,
+      required this.section_type});
 
   Map<String, dynamic> toJSON() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['section_on_site_uuid'] = section_on_site_uuid;
     data['section_name'] = section_name;
-
+    data['section_type'] = section_type;
     return data;
   }
 
@@ -18,32 +22,6 @@ class Section {
       : section_on_site_uuid = json.containsKey('section_on_site_uuid')
             ? json['form_on_site_uuid']
             : generateUUID(),
-        section_name = json['section_name'] as String;
-}
-
-Map<String, Section> ConvertJsonToMapSection(map) {
-  Map<String, Section> res = {};
-
-  map.forEach((key, value) {
-    Section s = Section(
-        section_name: value["section_name"],
-        section_on_site_uuid: value.containsKey('section_on_site_uuid')
-            ? value["section_on_site_uuid"]
-            : generateUUID());
-
-    res[key] = s;
-  });
-  // }
-  return res;
-}
-
-Map<String, dynamic> ConvertListSectionsToJson(Map<String, Section> map) {
-  Map<String, dynamic> res = {};
-
-  map.forEach((key, value) {
-    dynamic json = value.toJSON();
-    res[key] = json;
-  });
-  // }
-  return res;
+        section_name = json['section_name'] as String,
+        section_type = json['section_type'] as String;
 }

0077e36484e2fe598f6f89fa61f02cbde73074f6
Yannick Simon <yannick.simon@gmail.com>
2024-01-30 20:30:57 +0100

    refactoring

--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -1,12 +1,11 @@
 import 'dart:async';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 import '../models/model_user.dart';
 import '../network/api/login_api.dart';
 import '../network/api/user_api.dart';
 import 'widget/_homepage_authentifier_content.dart';
-import './utils/context.dart';
+import 'widget/_homepage_unauthentified_content.dart';
 import 'widget/app_bar.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,11 +16,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  LoginApi loginApi = LoginApi();
   UserApi userAPI = UserApi();
+  LoginApi loginApi = LoginApi();
 
   final String _title = 'sites';
   final String _currentTenant = 'ctei';
@@ -76,7 +72,8 @@ class _HomePageState extends State<HomePage> {
   Widget widgetBody(User me) {
     return me.isAuthorized()
         ? HomepageAuthentifiedContent(user: me)
-        : widgetLoginForm(context);
+        : HomepageUnAuthentifiedContent(
+            context: context, onConnexion: (value) => setState(() {}));
   }
 
   Scaffold widgetWaiting() {
@@ -91,101 +88,5 @@ class _HomePageState extends State<HomePage> {
 
   Scaffold widgetError() {
     return const Scaffold(body: Text("error"));
-  }
-
-  Widget widgetLoginForm(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.8,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            emailForm(),
-            const SizedBox(
-              height: 10,
-            ),
-            passwordForm(),
-            const SizedBox(
-              height: 10,
-            ),
-            submit(),
-            const SizedBox(
-              height: 80,
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget submit() {
-    return MaterialButton(
-        onPressed: () async {
-          String email = "";
-
-          email = emailController.text.trim();
-          email = email.replaceAll("\n", " ");
-          email = email.replaceAll("\r", " ");
-
-          String password = "";
-
-          password = passwordController.text.trim();
-          password = password.replaceAll("\n", " ");
-          password = password.replaceAll("\r", " ");
-
-          try {
-            Response result =
-                await loginApi.login(email: email, password: password);
-
-            if (result.statusCode == 401) {
-              if (!context.mounted) {
-                return;
-              }
-              context.showErrorSnackBar("Echec d'authentification");
-              return;
-            }
-
-            setState(() {});
-          } catch (e) {
-            if (!context.mounted) {
-              return;
-            }
-            context.showErrorSnackBar("Echec d'authentification $e");
-            return;
-          }
-        },
-        color: Theme.of(context).colorScheme.inversePrimary,
-        textColor: Colors.white,
-        child: const Text("Submit"));
-  }
-
-  Widget emailForm() {
-    return TextFormField(
-        controller: emailController,
-        decoration: const InputDecoration(hintText: "email"),
-        validator: validateEmail);
-  }
-
-  Widget passwordForm() {
-    return TextFormField(
-      controller: passwordController,
-      obscureText: true,
-      decoration: const InputDecoration(hintText: "Password"),
-    );
-  }
-
-  String? validateEmail(String? value) {
-    const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
-        r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
-        r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
-        r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
-        r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
-        r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
-        r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
-    final regex = RegExp(pattern);
-
-    return value!.isNotEmpty && !regex.hasMatch(value)
-        ? 'Enter a valid email address'
-        : null;
   }
 }
---
+++ lib/ui/widget/_homepage_unauthentified_content.dart
@@ -0,0 +1,133 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import '../utils/context.dart';
+
+import '../../network/api/login_api.dart';
+
+class HomepageUnAuthentifiedContent extends StatefulWidget {
+  const HomepageUnAuthentifiedContent({
+    super.key,
+    required this.context,
+    required this.onConnexion,
+  });
+
+  final BuildContext context;
+  final Function(int) onConnexion;
+
+  @override
+  State<HomepageUnAuthentifiedContent> createState() =>
+      _HomepageUnAuthentifiedContentState();
+}
+
+class _HomepageUnAuthentifiedContentState
+    extends State<HomepageUnAuthentifiedContent> {
+  LoginApi loginApi = LoginApi();
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widgetLoginForm(context),
+    );
+    // This trailing comma makes auto-formatting nicer for build methods.
+  }
+
+  Widget widgetLoginForm(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            emailForm(),
+            const SizedBox(
+              height: 10,
+            ),
+            passwordForm(),
+            const SizedBox(
+              height: 10,
+            ),
+            submit(),
+            const SizedBox(
+              height: 80,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget submit() {
+    return MaterialButton(
+        onPressed: () async {
+          String email = "";
+
+          email = emailController.text.trim();
+          email = email.replaceAll("\n", " ");
+          email = email.replaceAll("\r", " ");
+
+          String password = "";
+
+          password = passwordController.text.trim();
+          password = password.replaceAll("\n", " ");
+          password = password.replaceAll("\r", " ");
+
+          try {
+            Response result =
+                await loginApi.login(email: email, password: password);
+
+            if (result.statusCode == 401) {
+              if (!context.mounted) {
+                return;
+              }
+              context.showErrorSnackBar("Echec d'authentification");
+              return;
+            }
+            widget.onConnexion(1);
+
+            // setState(() {});
+          } catch (e) {
+            if (!context.mounted) {
+              return;
+            }
+            context.showErrorSnackBar("Echec d'authentification $e");
+            return;
+          }
+        },
+        color: Theme.of(context).colorScheme.inversePrimary,
+        textColor: Colors.white,
+        child: const Text("Submit"));
+  }
+
+  Widget emailForm() {
+    return TextFormField(
+        controller: emailController,
+        decoration: const InputDecoration(hintText: "email"),
+        validator: validateEmail);
+  }
+
+  Widget passwordForm() {
+    return TextFormField(
+      controller: passwordController,
+      obscureText: true,
+      decoration: const InputDecoration(hintText: "Password"),
+    );
+  }
+
+  String? validateEmail(String? value) {
+    const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
+        r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
+        r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
+        r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
+        r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
+        r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
+        r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
+    final regex = RegExp(pattern);
+
+    return value!.isNotEmpty && !regex.hasMatch(value)
+        ? 'Enter a valid email address'
+        : null;
+  }
+}

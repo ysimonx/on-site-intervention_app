41ab1e16e070301edb6a7f955aafd1d3a96cf150
Yannick Simon <yannick.simon@gmail.com>
2024-02-06 19:29:01 +0100

    lists

--- lib/ui/lists_page.dart
+++ lib/ui/lists_page.dart
@@ -22,17 +22,19 @@ class ListsPage extends StatefulWidget {
 // Create a corresponding State class.
 class ListsPageState extends State<ListsPage> {
   late String _title = 'lists';
-  late List<String> listOfLists = [];
+  late Map<String, List<String>> dictOfLists = {};
 
   @override
   void initState() {
     super.initState();
     _title = "${widget.site!.name} : lists";
-    listOfLists = ["A"];
+    dictOfLists = {
+      "BATIMENTS": ["R1", "R2"]
+    };
   }
 
-  Future<List<String>> getMyInformations() async {
-    return listOfLists;
+  Future<Map<String, List<String>>> getMyInformations() async {
+    return dictOfLists;
   }
 
   @override
@@ -41,8 +43,8 @@ class ListsPageState extends State<ListsPage> {
         future: getMyInformations(),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (snapshot.hasData) {
-            listOfLists = snapshot.data;
-            return widgetBody(user: widget.user, list: listOfLists);
+            dictOfLists = snapshot.data;
+            return widgetBody(user: widget.user, dict_of_lists: dictOfLists);
           } else if (snapshot.hasError) {
             return widgetError(widget.user);
           } else {
@@ -60,21 +62,23 @@ class ListsPageState extends State<ListsPage> {
             : const BaseAppBar(title: "login"));
   }
 
-  Widget widgetBody({required User user, required List<String> list}) {
+  Widget widgetBody(
+      {required User user, required Map<String, List<String>> dict_of_lists}) {
     return Scaffold(
       appBar: widgetAppBar(user),
       body: widgetListOfListContent(
-          user: user,
-          list: list,
-          onRefresh: (valueint, valueString) => setState(() {
-                if (valueString != "") {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                        duration: const Duration(milliseconds: 100),
-                        content: Text(valueString)),
-                  );
-                }
-              })),
+        user: user,
+        dict_of_lists: dict_of_lists,
+        onRefresh: (valueint, valueString) => setState(() {
+          if (valueString != "") {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                  duration: const Duration(milliseconds: 100),
+                  content: Text(valueString)),
+            );
+          }
+        }),
+      ),
       floatingActionButton: fabNewList(context: context, callback: CB),
     );
   }
@@ -93,7 +97,6 @@ class ListsPageState extends State<ListsPage> {
       onPressed: () async {
         //  _showDialog(callback);
         _showDialog(callback: callback, site: widget.site);
-        CB("coucou");
       },
       child: const Icon(Icons.add),
     );
@@ -116,16 +119,23 @@ class ListsPageState extends State<ListsPage> {
 
   Widget widgetListOfListContent(
       {required User user,
-      required List<String> list,
-      required void Function(dynamic valueint, dynamic valueString)
-          onRefresh}) {
+      required void Function(dynamic valueint, dynamic valueString) onRefresh,
+      required Map<String, List<String>> dict_of_lists}) {
+    List<String> list = [];
+    dict_of_lists.forEach((key, value) {
+      list.add(key);
+    });
+
     return ListView.builder(
         padding: const EdgeInsets.all(8),
-        itemCount: listOfLists.length,
+        itemCount: list.length,
         itemBuilder: (BuildContext context, int index) {
+          List<String>? values = dict_of_lists[list[index]];
+
           return SizedBox(
             height: 50,
-            child: Center(child: Text('Entry ${listOfLists[index]}')),
+            child:
+                Center(child: Text('Entry ${list[index]}: ${values!.length}')),
           );
         });
   }
@@ -134,8 +144,10 @@ class ListsPageState extends State<ListsPage> {
     required void Function(String message) callback,
     required Site? site,
   }) {
-    late TextEditingController textListController = TextEditingController();
+    late TextEditingController controllerListName = TextEditingController();
+    late TextEditingController controllerValues = TextEditingController();
     List<String> listValues = ["val1", "val2"];
+    controllerValues.text = listValues.join("\n");
 
     showDialog<void>(
       useRootNavigator: false,
@@ -148,21 +160,31 @@ class ListsPageState extends State<ListsPage> {
                       builder: (BuildContext context, StateSetter setState) {
                     return Column(children: [
                       TextField(
-                        controller: textListController,
+                        controller: controllerListName,
                         autofocus: true,
                         decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Color(0xFFF2F2F2),
                             hintText: "Enter the name of this new list"
                                 .toCapitalized()),
                       ),
-                      SizedBox(
-                          width: constrains.maxWidth * .8,
-                          height: constrains.maxHeight * .3, //
-                          child: ListView.builder(
-                              itemCount: listValues.length,
-                              itemBuilder: (_, index) {
-                                String value = listValues[index];
-                                return Text(value);
-                              }))
+                      const SizedBox(height: 10),
+                      TextFormField(
+                        controller: controllerValues,
+                        autofocus: true,
+                        autocorrect: false,
+                        keyboardType: TextInputType.multiline,
+                        minLines: 5,
+                        maxLines: 10,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Color(0xFFF2F2F2),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(4)),
+                            borderSide: BorderSide(width: 1),
+                          ),
+                        ),
+                      ),
                     ]);
                   }),
                   actions: <Widget>[
@@ -182,30 +204,9 @@ class ListsPageState extends State<ListsPage> {
                       child: const Text('Ok'),
                       onPressed: () async {
                         Navigator.pop(context);
-                        /*
-                        
-                        String email = textListController.text;
-                        List<String> roles_id = [];
-
-                        dictSiteRoles.forEach((key, value) {
-                          if (value) {
-                            roles_id.add(key);
-                          }
-                        });
-                        Response response = await siteApi.AddUserRoles(
-                            site_id: s.id, email: email, roles_id: roles_id);
-
-                        if (response.statusCode == 200) {
-                          Navigator.pop(context);
-                          callback("Processing Data");
-                          return;
-                        }
-                        if (response.statusCode == 400) {
-                          Navigator.pop(context);
-                          callback(
-                              "Processing Data Error ${response.data["error"]}");
-                          return;
-                        }*/
+                        dictOfLists[controllerListName.text] =
+                            controllerValues.text.split("\n");
+                        callback(controllerListName.text);
                       },
                     ),
                   ],

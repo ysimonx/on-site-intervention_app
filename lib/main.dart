8ef1d59fc750ab2f714acdfaf427cd52479ea842
Yannick Simon <yannick.simon@gmail.com>
2024-02-08 22:09:12 +0100

    googlefont

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'ui/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'On Site Interventions',
       theme: ThemeData(
+        fontFamily: GoogleFonts.inter().fontFamily,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
         useMaterial3: true,
       ),

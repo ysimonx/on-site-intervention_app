f9bdd89f24f79755d8a055c9921a6fa07f11d006
Yannick Simon <yannick.simon@gmail.com>
2024-02-10 10:44:09 +0100

    wip

--- lib/ui/widget/_sites_page.dart
+++ lib/ui/widget/_sites_page.dart
@@ -16,25 +16,31 @@ Widget getSitesWidget(
 
   return Column(children: <Widget>[
     Expanded(
-        child: ListTileTheme(
-      contentPadding: const EdgeInsets.all(15),
-      style: ListTileStyle.list,
-      dense: true,
-      child: ListView.builder(
-        itemCount: sites.length,
-        itemBuilder: (_, index) => Card(
-          margin: const EdgeInsets.all(10),
-          child: ListTile(
-            leading: const Icon(Icons.business),
-            title: Text(sites[index].name.toUpperCase()),
-            subtitle: Text(
-                "Organization : ${sites[index].tenant.name.toUpperCase()}",
-                style: TextStyle(fontSize: ThemeSize.text(xs))),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(sites[index].getRoleNamesForUser(user).join(", ")),
+      child: ListTileTheme(
+        contentPadding: const EdgeInsets.all(15),
+        style: ListTileStyle.list,
+        dense: true,
+        child: ListView.builder(
+          itemCount: sites.length,
+          itemBuilder: (_, index) => Card(
+            margin: const EdgeInsets.all(10),
+            child: ListTile(
+              leading: const Icon(Icons.business, size: 40),
+              title: Text(sites[index].name.toUpperCase()),
+              subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                        "Organization : ${sites[index].tenant.name.toUpperCase()}",
+                        style: TextStyle(fontSize: ThemeSize.text(xs))),
+                    Text(
+                        "Roles : ${sites[index].getRoleNamesForUser(user).join(", ")}",
+                        style: TextStyle(fontSize: ThemeSize.text(xs))),
+                  ]),
+              trailing: Row(mainAxisSize: MainAxisSize.min, children: [
                 IconButton(
+                    iconSize: 30,
                     onPressed: () async {
                       await Navigator.push(
                           context,
@@ -43,12 +49,12 @@ Widget getSitesWidget(
                                   SitePage(user: user, site: sites[index])));
                       onRefresh(1);
                     },
-                    icon: const Icon(Icons.navigate_next)),
-              ],
+                    icon: const Icon(Icons.navigate_next))
+              ]),
             ),
           ),
         ),
       ),
-    ))
+    )
   ]);
 }
